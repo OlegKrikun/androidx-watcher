18d0b247c7339e7bab3856c1aa461f8c724ea6df
OlegKrikun <oleg.krikun@gmail.com>
2019-10-23 23:59:20 +0300

    Stop using shadow

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,6 @@ plugins {
     id("org.jetbrains.kotlin.jvm") version "1.3.50"
     id("org.jetbrains.kotlin.plugin.serialization") version "1.3.50"
     id("nebula.deb") version "7.5.0"
-    id("com.github.johnrengelman.shadow") version "5.1.0"
 }
 
 repositories { jcenter() }
@@ -20,10 +19,13 @@ tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
 }
 
+val libs: Set<File> = configurations.getByName("runtimeClasspath").files
 tasks.withType(Jar::class) {
-    manifest.attributes("Main-Class" to "MainKt")
+    manifest.attributes(
+        "Main-Class" to "MainKt",
+        "Class-Path" to libs.joinToString(" ") { "lib/${it.name}" }
+    )
 }
-
 tasks.create("deb", Deb::class) {
     group = "build"
     version = "0.1.0"
@@ -33,9 +35,12 @@ tasks.create("deb", Deb::class) {
     preUninstall(file("scripts/preUninstall.sh"))
     postUninstall(file("scripts/postUninstall.sh"))
 
-    from(tasks["shadowJar"].outputs.files) { into("/opt/androidx-watcher") }
-    from(File(project.rootDir, "androidx-watcher.service")) { into("/opt/androidx-watcher") }
+    from(tasks["jar"].outputs.files) { into("/opt/androidx-watcher") }
+
+    from(libs) { into("/opt/androidx-watcher/lib") }
+
+    from(File(project.rootDir, "androidx-watcher.service")) { into("/opt/androidx-watcher/service") }
     from(File(project.rootDir, "androidx-watcher.properties.example")) { into("/opt/androidx-watcher") }
 
-    link("/etc/systemd/system/androidx-watcher.service", "/opt/androidx-watcher/androidx-watcher.service")
+    link("/etc/systemd/system/androidx-watcher.service", "/opt/androidx-watcher/service/androidx-watcher.service")
 }
