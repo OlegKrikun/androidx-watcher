1c3e7daaa233ed2802609b807370cb94502802c1
OlegKrikun <oleg.krikun@gmail.com>
2019-10-24 04:58:10 +0300

    Update artifact rendering

--- src/main/kotlin/artifact.kt
+++ src/main/kotlin/artifact.kt
@@ -10,7 +10,97 @@ data class Artifact(
     @Transient
     val id = "$group:$name:$version"
     @Transient
-    val groupSuffix = group.removePrefix("androidx.")
+    val key = keyOf(this)
 
     override fun toString() = id
+
+    data class Key(val title: String, val link: String, val version: String)
+}
+
+private fun keyOf(a: Artifact): Artifact.Key = when {
+    a.group == "androidx.annotation" && a.name.startsWith("annotation-experimental") -> a.createKey(
+        "Annotation Experimental",
+        "https://developer.android.com/jetpack/androidx/releases/annotation#experimental-${a.version}"
+    )
+    a.group == "androidx.arch.core" -> a.createKey(
+        "Arch",
+        "https://developer.android.com/jetpack/androidx/releases/arch#${a.version}"
+    )
+    a.group == "androidx.camera" -> a.createKey(
+        "CameraX",
+        "https://developer.android.com/jetpack/androidx/releases/camera#camera2-core-${a.version}"
+    )
+    a.group == "androidx.car" && a.name == "car-cluster" -> a.createKey(
+        "Car",
+        "https://developer.android.com/jetpack/androidx/releases/car#car-cluster-${a.version}"
+    )
+    a.group == "androidx.compose" -> a.createKey(
+        "Jetpack Compose",
+        "https://developer.android.com/jetpack/compose"
+    )
+    a.group == "androidx.concurrent" && a.name.startsWith("concurrent-listenablefuture") -> a.createKey(
+        "Concurrent",
+        "https://developer.android.com/jetpack/androidx/releases/concurrent#concurrent-listenableFuture-${a.version}"
+    )
+    a.group.startsWith("androidx.test.espresso") -> a.createKey(
+        "Test: Espresso",
+        "https://developer.android.com/jetpack/androidx/releases/test"
+    )
+    a.group == "androidx.test.ext" && a.name.startsWith("junit") -> a.createKey(
+        "Test: Junit",
+        "https://developer.android.com/jetpack/androidx/releases/test"
+    )
+    a.group == "androidx.test.ext" && a.name == "truth" -> a.createKey(
+        "Test: Truth",
+        "https://developer.android.com/jetpack/androidx/releases/test"
+    )
+    a.group == "androidx.test.janktesthelper" -> a.createKey(
+        "Test: Janktesthelper",
+        "https://developer.android.com/jetpack/androidx/releases/test"
+    )
+    a.group == "androidx.test.services" -> a.createKey(
+        "Test: Service",
+        "https://developer.android.com/jetpack/androidx/releases/test"
+    )
+    a.group == "androidx.test.uiautomator" -> a.createKey(
+        "Test: Uiautomator",
+        "https://developer.android.com/jetpack/androidx/releases/test"
+    )
+    a.group == "androidx.test" && a.name.startsWith("core") -> a.createKey(
+        "Test: Core",
+        "https://developer.android.com/jetpack/androidx/releases/test"
+    )
+    a.group == "androidx.test" && a.name == "monitor" -> a.createKey(
+        "Test: Monitor",
+        "https://developer.android.com/jetpack/androidx/releases/test"
+    )
+    a.group == "androidx.test" && a.name == "orchestrator" -> a.createKey(
+        "Test: Orchestrator",
+        "https://developer.android.com/jetpack/androidx/releases/test"
+    )
+    a.group == "androidx.test" && a.name == "rules" -> a.createKey(
+        "Test: Rules",
+        "https://developer.android.com/jetpack/androidx/releases/test"
+    )
+    a.group == "androidx.test" && a.name == "runner" -> a.createKey(
+        "Test: Runner",
+        "https://developer.android.com/jetpack/androidx/releases/test"
+    )
+    a.group == "androidx.ui" -> a.createKey(
+        "Jetpack Compose UI",
+        "https://developer.android.com/jetpack/compose"
+    )
+    a.group == "androidx.work" -> a.createKey("WorkManager")
+    else -> a.createKey()
+}
+
+private fun Artifact.createKey(name: String? = null, link: String? = null) = when {
+    name == null || link == null -> group.removePrefix("androidx.").let { groupSuffix ->
+        Artifact.Key(
+            name ?: groupSuffix.capitalize(),
+            link ?: "https://developer.android.com/jetpack/androidx/releases/$groupSuffix#$version",
+            version
+        )
+    }
+    else -> Artifact.Key(name, link, version)
 }
--- src/main/kotlin/render.kt
+++ src/main/kotlin/render.kt
@@ -1,107 +1,33 @@
+private const val MAX_MESSAGE_LENGTH = 4096
+
 private const val SEPARATOR = "\n"
-private const val TELEGRAM_MAX_UTF8_CHARS = 4096
+private const val PREFIX = "```\n"
+private const val SUFFIX = "\n```"
+private const val TRUNCATED_SUFFIX = "...$SUFFIX"
 
-fun List<Artifact>.render() = groupBy { it.key }.flatMap { it.render() }
+fun List<Artifact>.render() = groupBy { it.key }.map { it.render() }
 
-private fun Map.Entry<Key, List<Artifact>>.render() = value.fold(listOf(key.render())) { list, item ->
-    list.dropLast(1) + list.last().let { last ->
-        val text = item.render().also {
-            check(it.length < TELEGRAM_MAX_UTF8_CHARS) { "artifact render too long: $it" }
-        }
-        if (last.length + SEPARATOR.length + text.length < TELEGRAM_MAX_UTF8_CHARS) {
-            listOf(last + SEPARATOR + text)
-        } else {
-            listOf(last, text)
+private fun Map.Entry<Artifact.Key, List<Artifact>>.render(): String {
+    val title = "${key.version.marker()} *${key.title} ${key.version}* [»](${key.link})$SEPARATOR"
+    val body = value.joinToString(SEPARATOR, PREFIX, SUFFIX) { it.id }
+    val resultLength = title.length + body.length
+    return when {
+        resultLength <= MAX_MESSAGE_LENGTH -> title + body
+        else -> {
+            title + body.dropLast(resultLength + TRUNCATED_SUFFIX.length - MAX_MESSAGE_LENGTH) + TRUNCATED_SUFFIX
         }
     }
 }
 
-private fun Artifact.render() = "`$id`"
+private const val ICON_RELEASE = "\uD83C\uDF89"
+private const val ICON_CANDIDATE = "\uD83D\uDCA1"
+private const val ICON_UNSTABLE = "⚙"
 
-private fun Key.render() = "*$name $version* [»]($link)"
+private val releaseRegex = "^\\d+\\.\\d+\\.\\d+\$".toRegex()
+private val candidateRegex = "^.+rc\\d{2}\$".toRegex()
 
-private data class Key(val name: String, val link: String, val version: String)
-
-private val Artifact.key: Key
-    get() = when {
-        group == "androidx.annotation" && name.startsWith("annotation-experimental") -> createKey(
-            "Annotation Experimental",
-            "https://developer.android.com/jetpack/androidx/releases/annotation#experimental-$version"
-        )
-        group == "androidx.arch.core" -> createKey(
-            "Arch",
-            "https://developer.android.com/jetpack/androidx/releases/arch#$version"
-        )
-        group == "androidx.camera" -> createKey(
-            "CameraX",
-            "https://developer.android.com/jetpack/androidx/releases/camera#camera2-core-$version"
-        )
-        group == "androidx.car" && name == "car-cluster" -> createKey(
-            "Car",
-            "https://developer.android.com/jetpack/androidx/releases/car#car-cluster-$version"
-        )
-        group == "androidx.compose" -> createKey(
-            "Jetpack Compose",
-            "https://developer.android.com/jetpack/compose"
-        )
-        group == "androidx.concurrent" && name.startsWith("concurrent-listenablefuture") -> createKey(
-            "Concurrent",
-            "https://developer.android.com/jetpack/androidx/releases/concurrent#concurrent-listenableFuture-$version"
-        )
-        group.startsWith("androidx.test.espresso") -> createKey(
-            "Test: Espresso",
-            "https://developer.android.com/jetpack/androidx/releases/test"
-        )
-        group == "androidx.test.ext" && name.startsWith("junit") -> createKey(
-            "Test: Junit",
-            "https://developer.android.com/jetpack/androidx/releases/test"
-        )
-        group == "androidx.test.ext" && name == "truth" -> createKey(
-            "Test: Truth",
-            "https://developer.android.com/jetpack/androidx/releases/test"
-        )
-        group == "androidx.test.janktesthelper" -> createKey(
-            "Test: Janktesthelper",
-            "https://developer.android.com/jetpack/androidx/releases/test"
-        )
-        group == "androidx.test.services" -> createKey(
-            "Test: Service",
-            "https://developer.android.com/jetpack/androidx/releases/test"
-        )
-        group == "androidx.test.uiautomator" -> createKey(
-            "Test: Uiautomator",
-            "https://developer.android.com/jetpack/androidx/releases/test"
-        )
-        group == "androidx.test" && name.startsWith("core") -> createKey(
-            "Test: Core",
-            "https://developer.android.com/jetpack/androidx/releases/test"
-        )
-        group == "androidx.test" && name == "monitor" -> createKey(
-            "Test: Monitor",
-            "https://developer.android.com/jetpack/androidx/releases/test"
-        )
-        group == "androidx.test" && name == "orchestrator" -> createKey(
-            "Test: Orchestrator",
-            "https://developer.android.com/jetpack/androidx/releases/test"
-        )
-        group == "androidx.test" && name == "rules" -> createKey(
-            "Test: Rules",
-            "https://developer.android.com/jetpack/androidx/releases/test"
-        )
-        group == "androidx.test" && name == "runner" -> createKey(
-            "Test: Runner",
-            "https://developer.android.com/jetpack/androidx/releases/test"
-        )
-        group == "androidx.ui" -> createKey(
-            "Jetpack Compose UI",
-            "https://developer.android.com/jetpack/compose"
-        )
-        group == "androidx.work" -> createKey("WorkManager")
-        else -> createKey()
-    }
-
-private fun Artifact.createKey(
-    name: String = groupSuffix.capitalize(),
-    link: String = "https://developer.android.com/jetpack/androidx/releases/$groupSuffix#$version",
-    ver: String = version
-) = Key(name, link, ver)
+private fun String.marker() = when {
+    matches(releaseRegex) -> ICON_RELEASE
+    matches(candidateRegex) -> ICON_CANDIDATE
+    else -> ICON_UNSTABLE
+}
